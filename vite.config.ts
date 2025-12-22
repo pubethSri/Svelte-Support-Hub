f906525945f20a3fbf9a526e91bed3e928c99027
FriedStan <66070030@kmitl.ac.th>
2025-12-22 09:54:28 +0700

    update: remove some unnecessary things

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
-import path from "path";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
-	resolve: {
-    alias: {
-      $lib: path.resolve("./src/lib"),
-    },
-  },
 });
