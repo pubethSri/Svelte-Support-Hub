7a42d010c92d76fabd78c05f914b5873374fb694
FriedStan <66070030@kmitl.ac.th>
2026-01-19 08:50:31 +0700

    fix: header user value

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -18,12 +18,12 @@
 
     function canViewAdmin(user: any) {
         if (!user) return false;
-        return user.value.name.toLowerCase() === 'mr.jirathip kapanya'
-            || user.value.role.toLowerCase() === 'lecturer'
-            || user.value.name.toLowerCase() === 'montree kingkaew'
-            || user.value.name.toLowerCase() === 'mr.pubeth sriwattana'
-            || user.value.name.toLowerCase() === 'นายจารุกิตติ์ ศรีพาเพลิน'
-            || user.value.name.toLowerCase() === 'นายชญานนท์ สุภากิจ';
+        return user.name.toLowerCase() === 'mr.jirathip kapanya'
+            || user.role.toLowerCase() === 'lecturer'
+            || user.name.toLowerCase() === 'montree kingkaew'
+            || user.name.toLowerCase() === 'mr.pubeth sriwattana'
+            || user.name.toLowerCase() === 'นายจารุกิตติ์ ศรีพาเพลิน'
+            || user.name.toLowerCase() === 'นายชญานนท์ สุภากิจ';
     }
 
 </script>
